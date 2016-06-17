870f26014dec3faa4a657256342d19ceb30e8315
David Louie <dlouie1492@gmail.com>
2016-06-17 15:45:20 -0400

    Get microphone permissions

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
-$(document).ready(function({
-  
-}))
+$(document).ready(function(){
+
+    $("#start").click(function(){
+      startRecording()
+    })
+
+    $("#stop").click(function(){
+      stopRecording()
+    })
+
+      var onFail = function(e) {
+        console.log('Rejected!', e);
+      };
+
+      var onSuccess = function(s) {
+        stream = s;
+      }
+
+      window.URL = window.URL || window.webkitURL;
+      navigator.getUserMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+      var stream;
+      var audio = document.querySelector('audio');
+
+      function startRecording() {
+        if (navigator.getUserMedia) {
+          navigator.getUserMedia({audio: true}, onSuccess, onFail);
+        } else {
+          console.log('navigator.getUserMedia not present');
+        }
+      }
+
+      function stopRecording() {
+        audio.src = window.URL.createObjectURL(stream);
+      }
+
+    function hasGetUserMedia(){
+      return !!(navigator.getUserMedia ||navigator.webkitGetUserMedia || navigator.mozGetUzerMedia || navigator.msGetUserMedia)
+    }
+
+    if (!hasGetUserMedia()){
+      alert("NOT GONNA BE ABLE TO DO IT")
+    }
+  })
